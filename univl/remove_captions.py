7ef36e6bb6404d2e76c93e1411c9f59ac5095cd4
Jason Cheng <dumplinglife7584@gmail.com>
2023-09-26 21:52:05 -0400

    add command line start/end

--- univl/remove_captions.py
+++ univl/remove_captions.py
@@ -1,12 +1,18 @@
 import pickle
 import numpy as np
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--start', type=int)
+parser.add_argument('--end', type=int)
+
+args = parser.parse_args()
 
 with open("data/youcookii/youcookii_data.no_transcript.pickle", 'rb') as infile:
     data = pickle.load(infile)
     for k, v in data.items():
-        # fill it with some stuff
-        data[k]["start"] = np.array([20])
-        data[k]["end"] = np.array([30])
+        data[k]["start"] = np.array([args.start])
+        data[k]["end"] = np.array([args.end])
         data[k]["text"] = np.array(["cut"]) # use a real word to be safe
 
 with open("data/youcookii/youcookii_data_no_captions.no_transcript.pickle", 'wb') as outfile:
