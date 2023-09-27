7d99cab57887375c7bceb959ac5ab2e75e2cb77a
Jason Cheng <dumplinglife7584@gmail.com>
2023-09-26 21:54:03 -0400

    add print(data)

--- univl/view_data.py
+++ univl/view_data.py
@@ -5,6 +5,6 @@ with open("data/youcookii/youcookii_data.no_transcript.pickle", 'rb') as file:
 with open("data/youcookii/youcookii_videos_features.pickle", 'rb') as file:
     features = pickle.load(file)
 
-print()
+print(data)
 
 # zuDJIPoSl7o
