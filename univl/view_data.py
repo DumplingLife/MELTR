c7a7ade122769cc84a491b35fd4cf7bea2f71052
Jason Cheng <dumplinglife7584@gmail.com>
2023-09-26 21:40:11 -0400

    Update view_data.py

--- univl/view_data.py
+++ univl/view_data.py
@@ -1,6 +1,6 @@
 import pickle
 
-with open("data/youcookii/youcookii_data_no_captions.no_transcript.pickle", 'rb') as file:
+with open("data/youcookii/youcookii_data.no_transcript.pickle", 'rb') as file:
     data = pickle.load(file)
 with open("data/youcookii/youcookii_videos_features.pickle", 'rb') as file:
     features = pickle.load(file)
