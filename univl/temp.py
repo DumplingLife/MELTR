7eca090ccccbbc91154b785b3214099507284d07
Jason Cheng <dumplinglife7584@gmail.com>
2023-09-26 20:22:05 -0400

    Create temp.py

---
+++ univl/temp.py
@@ -0,0 +1,10 @@
+import pickle
+
+with open("data/youcookii/youcookii_data.no_transcript.pickle", 'rb') as file:
+    data = pickle.load(file)
+with open("data/youcookii/youcookii_videos_features.pickle", 'rb') as file:
+    features = pickle.load(file)
+
+print()
+
+# zuDJIPoSl7o
