e431a4c19609949e22315ef777aad748b7b3d9cf
Jason Cheng <dumplinglife7584@gmail.com>
2023-09-26 21:19:05 -0400

    add filler captions

--- univl/remove_captions.py
+++ univl/remove_captions.py
@@ -4,9 +4,10 @@ import numpy as np
 with open("data/youcookii/youcookii_data.no_transcript.pickle", 'rb') as infile:
     data = pickle.load(infile)
     for k, v in data.items():
-        data[k]["start"] = np.array([])
-        data[k]["end"] = np.array([])
-        data[k]["text"] = np.array([])
+        # fill it with some stuff
+        data[k]["start"] = np.array([10])
+        data[k]["end"] = np.array([20])
+        data[k]["text"] = np.array(["cut"]) # use a real word to be safe
 
 with open("data/youcookii/youcookii_data_no_captions.no_transcript.pickle", 'wb') as outfile:
     pickle.dump(data, outfile)
