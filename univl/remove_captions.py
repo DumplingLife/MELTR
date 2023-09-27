d4108bc087a318281deaa0cec85bddbed02242ac
Jason Cheng <dumplinglife7584@gmail.com>
2023-09-26 21:40:25 -0400

    changed timestamps for testing

--- univl/remove_captions.py
+++ univl/remove_captions.py
@@ -5,8 +5,8 @@ with open("data/youcookii/youcookii_data.no_transcript.pickle", 'rb') as infile:
     data = pickle.load(infile)
     for k, v in data.items():
         # fill it with some stuff
-        data[k]["start"] = np.array([10])
-        data[k]["end"] = np.array([20])
+        data[k]["start"] = np.array([20])
+        data[k]["end"] = np.array([30])
         data[k]["text"] = np.array(["cut"]) # use a real word to be safe
 
 with open("data/youcookii/youcookii_data_no_captions.no_transcript.pickle", 'wb') as outfile:
