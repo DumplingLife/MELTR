e1799ab9a316a9fc4370372ca6d4579bd6ccb190
Jason Cheng <dumplinglife7584@gmail.com>
2023-10-02 12:00:53 -0400

    Update features_to_dict.py

--- univl/features_to_dict.py
+++ univl/features_to_dict.py
@@ -4,7 +4,8 @@ import pickle
 input_file = "data/actionsense/actionsense_features_array.pickle"
 output_file = "data/actionsense/actionsense_features.pickle"
 
-data = np.load(input_file)
-new_data = {"S03_split2": data}
+with open(input_file, "rb") as file:
+    features = pickle.load(file)
+new_data = {"S03_split2": features}
 with open(output_file, 'wb') as f:
     pickle.dump(new_data, f)
